e32edc60465b8aaaf8e4c77e81f3d8dd9a56df9e
Holok Chen <>
2021-07-16 22:25:04 +0800

    ex6

---
+++ app/ex6.hs
@@ -0,0 +1,13 @@
+-- apply plus5 to 53:
+plus5 53
+
+-- apply plus5 to
+-- the application of plus5 to 6
+plus5 (plus5 6)
+
+(+6) = \x -> x+6
+(7+) = \y -> 7+y
+f(x) = ....
+f = \x -> ....
+
+plus5 :: Int -> Int
